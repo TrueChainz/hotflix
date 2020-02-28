d27c58f1d4b085992847fb0d2931f72efef8ed21
Adnan <adnan.aant@gmail.com>
2020-02-28 00:53:53 +0000

    Updated movie component

--- src/components/Main/Movie/Movie.js
+++ src/components/Main/Movie/Movie.js
@@ -1,14 +1,33 @@
-import React, { useEffect } from "react"
+import React, { useState ,useEffect } from "react"
 import './Movie.css'
+import MovieInfo from "../MovieInfo/MovieInfo"
 
 const Movie = ({ movie }) => {
+    const [display, setDisplay] = useState(false)
+
+    const getDisplay = () => {
+        if (display === true) {
+            return null
+        } else {
+            setDisplay(true)
+        }
+    }
+
+    const closeDisplay = () => {
+        setDisplay(false)
+    }
+
     return (
-        <div className="Movie">
-            <img src={movie.image} alt='Movie image'/>
-            <div className='title'>{movie.title}</div>
-            {movie.rating >= 8 ? <div className='rating green'>{movie.rating}</div> : <div className='rating amber'>{movie.rating}</div>}
-            {/* <img src={}></img> */}
-        </div>
+        <>
+            <div className="Movie" onClick={getDisplay}>
+                <img src={movie.image} alt='Movie image'/>
+                <div className='title'>{movie.title}</div>
+                {movie.rating >= 8 ? <div className='rating green'>{movie.rating}</div> : <div className='rating amber'>{movie.rating}</div>}
+                {/* <img src={}></img> */}
+                {display && <MovieInfo id={movie.id} closeDisplay={closeDisplay}/>}
+            </div>
+            
+        </>
     )
 }
 
