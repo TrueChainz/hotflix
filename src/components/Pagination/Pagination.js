af26754742bc080dfdfd9e085d23d559402e5d02
CrampedAverage <adnan.aant@gmail.com>
2021-02-15 16:53:59 +0000

    Removed a lot of props and implemented redux recuers and actions to its event listeners

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Pagination.css'
 import '../../util/MovieDB'
+import { useDispatch} from 'react-redux'
+import { decrementPage, incrementPage, skipPageBack, skipPageForward } from '../../actions'
 
-const Pagination = ({decrement, increment, number, resetNumber, skip, last}) => {
+const Pagination = ({last, number}) => {
+    const dispatch = useDispatch();
 
     return (
         <div className="Whole">
@@ -10,11 +13,11 @@ const Pagination = ({decrement, increment, number, resetNumber, skip, last}) => 
                     {arrray.map(square =>  <div className='square'></div>)}
                 </div> */}
                 <div className='Pagination'>
-                    { number > 1 && <div className='arrow_two' onClick={resetNumber}> {`<<`} </div>}
-                    <div className='arrow' onClick={decrement}> {`<`} </div>
+                    { number > 1 && <div className='arrow_two' onClick={() => dispatch(skipPageBack())}> {`<<`} </div>}
+                    <div className='arrow' onClick={() => dispatch(decrementPage())}> {`<`} </div>
                     <div className='number'>{number}</div>
-                    <div className='arrow' onClick={increment}>{`>`}</div>
-                    {last != number && <div className='arrow_two' onClick={skip}>{`>>`}</div>}
+                    <div className='arrow' onClick={() => dispatch(incrementPage())}>{`>`}</div>
+                    {last != number && <div className='arrow_two' onClick={() => dispatch(skipPageForward())}>{`>>`}</div>}
                 </div>
         </div>
     )
