f2754882d6debea47703fcf49520054185e3348d
CrampedAverage <adnan.aant@gmail.com>
2021-03-06 19:08:34 +0000

    Moved these files to new folders

---
+++ src/components/Pages/Catalog/Catalog.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react'
+import './Catalog.css'
+import {titleStyle} from '../../../style'
+import { MovieDB } from '../../../util/MovieDB'
+import MovieList from '../../Main/MovieList/MovieList'
+import { useSelector } from 'react-redux'
+
+const Catalog = (props) => {
+
+    const pageNumber = useSelector(state => state.pageNum)
+    const [term, setTerm] = useState('')
+    const [border, setBorder] = useState('')
+    const [movies, setMovies] = useState([])
+    const [totalPage, setTotalPage] = useState(0)
+
+    const NavComponent = props.component[0]
+    const PaginationComponent = props.component[1]
+
+    const searchBoxStyle = {
+        border
+    }
+
+    const changeTerm = e => {
+        setTerm(e.target.value)
+    }
+
+    const searchMovie = (term, pageNumber) => {
+        if (term.length === 0) {
+            alert('Please provide a search term')
+            return
+        }
+        MovieDB.searchMovie(term, pageNumber).then(list => setMovies(list))
+        MovieDB.numberOfSearchPages(term, pageNumber).then(num => setTotalPage(num))
+    }
+
+    const keypressFunction = e => {
+        if (e.key === 'Enter') {
+            searchMovie(term, pageNumber)
+        }
+    }
+
+    useEffect(() => {
+        if (term.length === 0) {
+            setBorder('')
+        } else {
+            setBorder('1px solid yellow')
+        }
+        
+    },[term])
+
+    useEffect(() => {
+        if (term.length > 0) {
+            MovieDB.searchMovie(term, pageNumber).then(list => setMovies(list))
+        }
+    }, [pageNumber])
+
+    useEffect(() => {
+        document.title = props.title
+    })
+
+    return (
+        <>
+            <NavComponent />
+            <div className='darken' />
+            <div className='Catalog'>
+                <div className="container" style={titleStyle}>
+                    <h1 className="home-title">Catalog</h1>
+                </div>
+            </div>
+            <div className='sort'>
+                <div className="container">
+                    <div className='catalog-sort'>
+                        <div className='search-field' style={searchBoxStyle} onKeyPress={keypressFunction}>
+                            <input type="text" placeholder='Search movies...' value={term} onChange={changeTerm}/>
+                            <button onClick={searchMovie.bind(this, term, pageNumber)} className='search-button'>
+                                <ion-icon name="search-outline"></ion-icon>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <MovieList movies={movies}/>
+            {totalPage > 0 && <PaginationComponent number={pageNumber} last={totalPage}/>}
+        </>
+    )
+}
+
+export default Catalog
